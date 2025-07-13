07bc0f2c8631fe56908945cf66bc254222a9253e
Ajinkya Kutarmare <160856721+Aj22122003@users.noreply.github.com>
2025-07-13 18:11:09 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message sent! (This is a placeholder. Integrate email functionality later.)");
+});
